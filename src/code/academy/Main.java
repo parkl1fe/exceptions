741a7f124a19a5f41b2774837859f783d31c1c47
sebpra <sebpra@gmail.com>
2020-07-13 19:10:03 +0300

    Task 2

---
+++ src/code/academy/Main.java
@@ -0,0 +1,10 @@
+package code.academy;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Input input = new Input();
+        System.out.println(input.returnInput());
+
+    }
+}
